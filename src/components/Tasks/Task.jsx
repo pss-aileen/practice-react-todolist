b988781adca9049765d9e8cdb6f9a5b718c0f4ad
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-09-15 16:18:13 +0900

    :construction: wip: styling basic Material UI #8
    まだいまいちなところあり。とりあえず。

--- src/components/Tasks/Task.jsx
+++ src/components/Tasks/Task.jsx
@@ -1,5 +1,13 @@
 import { useContext, useState } from 'react';
 import { TasksDispatchContext } from '../../context/TasksContext';
+import Checkbox from '@mui/material/Checkbox';
+import ListItem from '@mui/material/ListItem';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import SaveIcon from '@mui/icons-material/Save';
+import Box from '@mui/system/Box';
+import Input from '@mui/material/Input';
 
 export default function Task({ id, text, checked }) {
   const [isEditting, setIsEditting] = useState(false);
@@ -28,24 +36,38 @@ export default function Task({ id, text, checked }) {
   }
 
   return (
-    <li>
-      <div>
-        <input type='checkbox' checked={checked} onChange={handleChange} />
-        {isEditting ? <input type='text' value={newText} onChange={(e) => setNewText(e.target.value)} /> : <span>{text}</span>}
+    <ListItem disablePadding>
+      <Box sx={{ display: 'flex', flexGrow: 1, justifyContent: 'space-between', gap: '16px', alignItems: 'center' }}>
+        <Box sx={{ flex: 1, maxWidth: '100%', display: 'flex', alignItems: 'center' }}>
+          <Checkbox label={isEditting ? <input type='text' value={newText} onChange={(e) => setNewText(e.target.value)} /> : <span>{text}</span>} size='small' checked={checked} onChange={handleChange} />
+          {isEditting ? <Input type='text' value={newText} onChange={(e) => setNewText(e.target.value)} fullWidth /> : <span>{text}</span>}
+        </Box>
 
-        {isEditting ? <button onClick={() => handleEditDone()}>Edit Done</button> : <button onClick={() => handleEdit()}>Edit</button>}
+        <div>
+          {isEditting ? (
+            <IconButton aria-label='edit' onClick={() => handleEditDone()} size='small'>
+              <SaveIcon fontSize='small' />
+            </IconButton>
+          ) : (
+            <IconButton aria-label='edit' onClick={() => handleEdit()} size='small'>
+              <EditIcon fontSize='small' />
+            </IconButton>
+          )}
 
-        <button
-          onClick={() =>
-            dispatch({
-              type: 'delete',
-              id: id,
-            })
-          }
-        >
-          Delete
-        </button>
-      </div>
-    </li>
+          <IconButton
+            aria-label='delete'
+            onClick={() =>
+              dispatch({
+                type: 'delete',
+                id: id,
+              })
+            }
+            size='small'
+          >
+            <DeleteIcon fontSize='small' />
+          </IconButton>
+        </div>
+      </Box>
+    </ListItem>
   );
 }
--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -1,15 +1,16 @@
 import { useContext } from 'react';
 import Task from './Task';
 import { TasksContext } from '../../context/TasksContext';
+import List from '@mui/material/List';
 
 export default function Tasks() {
   const tasks = useContext(TasksContext);
 
   return (
-    <ul>
+    <List>
       {tasks.map((task) => {
         return <Task key={task.id} id={task.id} text={task.text} checked={task.checked} />;
       })}
-    </ul>
+    </List>
   );
 }
