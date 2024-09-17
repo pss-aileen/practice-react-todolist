af52d40ecfffcc4c32db35db8d2188fd48870b0a
Aileen <140990100+pss-aileen@users.noreply.github.com>
2024-09-17 10:25:15 +0900

    :cherry_blossom: feat: submit by pressing Enter on task edit from #13

--- src/components/Tasks/Task.jsx
+++ src/components/Tasks/Task.jsx
@@ -42,32 +42,45 @@ export default function Task({ id, text, checked }) {
   }
 
   return (
-    <ListItem disablePadding>
-      <Box sx={{ display: 'flex', flexGrow: 1, justifyContent: 'space-between', gap: '16px', alignItems: 'center' }}>
-        <Box sx={{ flex: 1, maxWidth: '100%', display: 'flex', alignItems: 'center' }}>
-          <Checkbox size='small' checked={checked} onChange={handleChange} id={id.toString()} />
-          <label htmlFor={id} style={{ width: '100%', cursor: 'pointer' }}>
-            {isEditting ? <Input type='text' value={newText} onChange={(e) => setNewText(e.target.value)} fullWidth /> : <span>{text}</span>}
-          </label>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleEditDone();
+      }}
+    >
+      <ListItem disablePadding>
+        <Box sx={{ display: 'flex', flexGrow: 1, justifyContent: 'space-between', gap: '16px', alignItems: 'center' }}>
+          <Box sx={{ flex: 1, maxWidth: '100%', display: 'flex', alignItems: 'center' }}>
+            <Checkbox size='small' checked={checked} onChange={handleChange} id={id.toString()} />
+            <label htmlFor={id} style={{ width: '100%', cursor: 'pointer' }}>
+              {isEditting ? <Input type='text' value={newText} onChange={(e) => setNewText(e.target.value)} fullWidth /> : <span>{text}</span>}
+            </label>
+          </Box>
+
+          <div>
+            {isEditting ? (
+              <IconButton
+                aria-label='save'
+                onClick={() => {
+                  handleEditDone();
+                }}
+                size='small'
+              >
+                <SaveIcon fontSize='small' />
+              </IconButton>
+            ) : (
+              <IconButton aria-label='edit' onClick={() => handleEdit()} size='small' type='button'>
+                <EditIcon fontSize='small' />
+              </IconButton>
+            )}
+
+            <IconButton aria-label='delete' onClick={() => handleDelete()} size='small' type='button'>
+              <DeleteIcon fontSize='small' />
+            </IconButton>
+          </div>
+          <Confirm id={id} text={text} isOpen={modalIsOpen} setIsOpen={setModalIsOpen} />
         </Box>
-
-        <div>
-          {isEditting ? (
-            <IconButton aria-label='edit' onClick={() => handleEditDone()} size='small'>
-              <SaveIcon fontSize='small' />
-            </IconButton>
-          ) : (
-            <IconButton aria-label='edit' onClick={() => handleEdit()} size='small'>
-              <EditIcon fontSize='small' />
-            </IconButton>
-          )}
-
-          <IconButton aria-label='delete' onClick={() => handleDelete()} size='small'>
-            <DeleteIcon fontSize='small' />
-          </IconButton>
-        </div>
-        <Confirm id={id} text={text} isOpen={modalIsOpen} setIsOpen={setModalIsOpen} />
-      </Box>
-    </ListItem>
+      </ListItem>
+    </form>
   );
 }
